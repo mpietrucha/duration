3dcd8e5aa3aaba6c7f7b831dd1c4be62cbe042a7
Michał <mpietrucha50@gmail.com>
2023-12-04 12:09:43 +0000

    wip

--- src/Duration.php
+++ src/Duration.php
@@ -2,6 +2,14 @@
 
 namespace Mpietrucha\Support;
 
+use Mpietrucha\Support\Concerns\Forwardable;
+
 class Duration
 {
+    use Forwardable;
+
+    public function __construct(int|DateInterval $duration = null)
+    {
+        // $this->forward()->to(Sleep::class);
+    }
 }
